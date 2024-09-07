5a4840f2351412a06d632b56bab633ae3ceec254
Noahjtremblay <noahjtremblay@gmail.com>
2024-09-07 18:32:53 -0400

    Essaie de ciblage de carte pour acf true/false

--- tp1/front-page.php
+++ tp1/front-page.php
@@ -15,30 +15,52 @@
             <?php the_content(); ?>
         </div>
     </div>
+
+    <?php
+
+            // Affiche 2 jeux de la catégorie favorie
+                $arg = array(
+                    'post_type' => 'recette',
+                    'post_status' => 'publish',
+                    'posts_per_page' => '2',
+                    'category_name' => 'plates-formes',
+                    
+                );
+                $query = new Wp_Query($arg);
+    ?>
+
     <?php if(get_field('favorite_title')) : ?>
         <div class="favoris">
             <div class="wrapper">
                 <h2><?php the_field('favorite_title')?></h2>
                 <div class="cards">
                     <div class="wrapper">
-                        <a href="">
-                            <div class="card">
-                                <div class="card_media"><img src="assets/images/recette01.jpg" alt="" /></div>
-                                <div class="card_content">
-                                    <h4>Plat principale</h4>
-                                    <h3>Tilapia aux épices</h3>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="">
-                            <div class="card">
-                                <div class="card_media"><img src="assets/images/recette01.jpg" alt="" /></div>
-                                <div class="card_content">
-                                    <h4>Plat principale</h4>
-                                    <h3>Tilapia aux épices</h3>
-                                </div>
-                            </div>
-                        </a>
+                        <?php  if ($query->have_posts()) : ?>
+                            <?php while ($query->have_posts()) : $query->the_post(); ?>
+                                <a href="<?php the_permalink(); ?>">
+                                    < class="card">
+                                        <div class="card_media"><?php 
+                                $image = get_field('thumbnail');
+                                if( !empty( $image ) ): ?>
+                                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                                <?php endif; ?></div>
+                                        <div class="card_content">
+                                            <?php $categories = array(); ?>
+                                            <?php foreach(get_the_category() as $category) : ?>
+                                                <?php array_push($categories, $category->name); ?>
+                                            <?php endforeach; ?>
+
+                                                
+                                            <?php if($categories) : ?>
+                                                <h4 class="categorie"><?php echo implode(', ', $categories); ?></h4>
+                                            <?php endif; ?>
+                                            <h3><?php the_title(); ?></h3>
+                                        </div>
+                                    
+                                </a>
+                            <?php endwhile ?>
+                        <?php endif ?>
+                        <?php wp_reset_postdata(); ?>
                     </div>
                 </div>
             </div>
