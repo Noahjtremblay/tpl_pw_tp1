c3a14821bc3a05ae15cfaf567282c3df63ba301b
Noahjtremblay <noahjtremblay@gmail.com>
2024-09-07 21:01:11 -0400

    La page 404 ne fonctionne toujours pas le reste est terminer, je crois.

--- tp1/footer.php
+++ tp1/footer.php
@@ -5,11 +5,11 @@
                 <nav>
                     <ul>
                         <?php while (have_rows('social_media','options')) : the_row();?>
-                            <a target="_blank" href="<?php the_sub_field('link','options') ?>" <?php the_sub_field('name','options') ?>>
-                                <li><svg class="icon icon--lg">
+                           <li> <a target="_blank" href="<?php the_sub_field('link','options') ?>" <?php the_sub_field('name','options') ?>>
+                                <svg class="icon icon--lg">
                                         <use xlink:href="#icon-<?php the_sub_field('name','options') ?>"></use>
-                                    </svg></li>
-                            </a> 
+                                    </svg>
+                            </a> </li>
                         <?php endwhile; ?>
                     </ul>
                 </nav>
--- tp1/index.php
+++ tp1/index.php
@@ -1,36 +1,18 @@
 <?php get_header(); ?>
+
     <div class="hero">
-        <img src="assets/images/hero.jpg" alt="" />
+    <?php the_post_thumbnail(); ?>
         <div class="wrapper">
-            <h1>À vos épices</h1>
-            <a href="liste-recettes.html" class="btn">Voir toutes les recettes</a>
+            <h1><?php the_title(); ?></h1>
+
+            
+            
+            <a href="<?php the_permalink(11)?>" class="btn">Voir toutes les recettes</a>
         </div>
     </div>
-    <div class="favoris">
+    <div class="content">
         <div class="wrapper">
-            <h2>Les recettes Coup de coeur</h2>
-            <div class="cards">
-                <div class="wrapper">
-                    <a href="">
-                        <div class="card">
-                            <div class="card_media"><img src="assets/images/recette01.jpg" alt="" /></div>
-                            <div class="card_content">
-                                <h4>Plat principale</h4>
-                                <h3>Tilapia aux épices</h3>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div class="card">
-                            <div class="card_media"><img src="assets/images/recette01.jpg" alt="" /></div>
-                            <div class="card_content">
-                                <h4>Plat principale</h4>
-                                <h3>Tilapia aux épices</h3>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            <?php the_content(); ?>
         </div>
     </div>
 
